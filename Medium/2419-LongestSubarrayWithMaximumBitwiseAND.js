11f59fe2160620b09998a8d1b37155395f7305cc
ABNmmd <moha.ww.ha@gmail.com>
2024-09-14 22:50:10 +0100

    Longest Subarray With Maximum Bitwise AND

---
+++ Medium/2419-LongestSubarrayWithMaximumBitwiseAND.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestSubarray = function (nums) {
+    let maxVal = 0;
+    let ans = 0;
+    let currentStreak = 0;
+
+    for (let num of nums) {
+        if (maxVal < num) {
+            maxVal = num;
+            ans = currentStreak = 0;
+        }
+        if (maxVal == num) {
+            currentStreak++;
+        } else {
+            currentStreak = 0;
+        }
+        ans = Math.max(ans, currentStreak);
+    }
+    return ans;
+};
