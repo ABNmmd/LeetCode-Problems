c9a4f9d33031fd878201911440fec9db45e80977
ABNmmd <moha.ww.ha@gmail.com>
2024-09-18 21:08:16 +0100

    Largest Number

---
+++ Medium/179-LargestNumber.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function (nums) {
+    let stringArr = nums.map(el => el.toString());
+    stringArr.sort(
+        (a, b) => {
+            return a + b < b + a ? 1 : -1;
+        }
+    );
+    return stringArr[0] !== '0' ? stringArr.join('') : '0';
+};
