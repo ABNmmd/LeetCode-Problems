253c62a85de20700e8765199af98b0cc550096c9
ABNmmd <moha.ww.ha@gmail.com>
2024-09-20 22:04:50 +0100

    Shortest Palindrome

---
+++ Hard/214-ShortestPalindrome.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var shortestPalindrome = function (s) {
+    let slen = s.length;
+    if (slen === 0) return '';
+    
+    let left = 0;
+    for (let right = slen-1; right >= 0; right-- ) {
+        if (s[right] == s[left]) left++;
+    }
+
+    if (left == slen) return s;
+    let sfx = s.substring(left)
+    let revSfx = sfx.split('').reverse().join('');
+    return revSfx + shortestPalindrome(s.substring(0, left)) + sfx;
+};
