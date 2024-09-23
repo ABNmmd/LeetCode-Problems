8150c0b5e0015efd0cd47d212ee4e9c9498830e9
ABNmmd <moha.ww.ha@gmail.com>
2024-09-23 23:30:01 +0100

    Extra Characters in a String

---
+++ Medium/2707-ExtraCharactersInAString.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string[]} dictionary
+ * @return {number}
+ */
+var minExtraChar = function (s, dictionary) {
+    const n = s.length;
+    const dicSet = new Set(dictionary);
+
+    let dp = new Array(n + 1).fill(0);
+
+    for (let st = n - 1; st >= 0; st--) {
+        let cur = '';
+        dp[st] = dp[st + 1] + 1;
+        for (let e = st; e < n; e++) {
+            cur += s[e];
+            if (dicSet.has(cur)) {
+                dp[st] = Math.min(dp[st], dp[e + 1]);
+            }
+        }
+    }
+    return dp[0];
+};
