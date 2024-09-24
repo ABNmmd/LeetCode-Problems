26780c38eee5e6425c9d19dda5a7ed4ee3017674
ABNmmd <moha.ww.ha@gmail.com>
2024-09-24 17:10:23 +0100

    Find the Length of the Longest Common Prefix

--- Medium/3043-FindTheLengthOftheLongestCommonPrefix.js
+++ Medium/3043-FindTheLengthOftheLongestCommonPrefix.js
@@ -3,21 +3,21 @@
  * @param {number[]} arr2
  * @return {number}
  */
-var longestCommonPrefix = function(arr1, arr2) {
+var longestCommonPrefix = function (arr1, arr2) {
     let arr1_prefixes = new Set();
 
-    for( val of arr1){
-        while(!arr1_prefixes.has(val) && val > 0){
+    for (val of arr1) {
+        while (!arr1_prefixes.has(val) && val > 0) {
             arr1_prefixes.add(val);
             val = Math.floor(val / 10);
         }
     }
     let longestPrefix = 0;
-    for( val of arr2){
-        while(!arr1_prefixes.has(val) && val > 0){
+    for (val of arr2) {
+        while (!arr1_prefixes.has(val) && val > 0) {
             val = Math.floor(val / 10);
         }
-        if (val > 0){
+        if (val > 0) {
             longestPrefix = Math.max(longestPrefix, String(val).length);
         }
     }
