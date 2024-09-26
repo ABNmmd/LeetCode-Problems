6b86df58399f6a122b642b262d914f68ba0d0145
ABNmmd <moha.ww.ha@gmail.com>
2024-09-26 23:51:12 +0100

    My Calendar I

---
+++ Medium/729-MyCalendarI.js
@@ -0,0 +1,23 @@
+
+var MyCalendar = function() {
+    this.calendar = [];
+};
+
+/** 
+ * @param {number} start 
+ * @param {number} end
+ * @return {boolean}
+ */
+MyCalendar.prototype.book = function(start, end) {
+    for (let [s, e] of this.calendar){
+        if (s < end && start < e) return false;
+    }
+    this.calendar.push([start, end]);
+    return true;
+};
+
+/** 
+ * Your MyCalendar object will be instantiated and called as such:
+ * var obj = new MyCalendar()
+ * var param_1 = obj.book(start,end)
+ */
