f9c643e8fe8522d50bbbcd261a7e34a5ef94a4a4
ABNmmd <moha.ww.ha@gmail.com>
2024-10-01 15:17:32 +0100

    Check If Array Pairs Are Divisible by k

---
+++ Medium/1497-CheckIfArrayPairsAreDivisibleByk.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {boolean}
+ */
+var canArrange = function(arr, k) {
+    let remainderCount = new Map();
+    for (let num of arr) {
+        let remainder = ((num % k) + k) % k;
+        remainderCount.set(remainder, (remainderCount.get(remainder) || 0) + 1);
+    }
+
+    for (let [remainder, count] of remainderCount) {
+        if (remainder === 0) {
+            if (count % 2 !== 0) return false;
+        } else {
+            let complement = k - remainder;
+            if (remainderCount.get(complement) !== count) return false;
+        }
+    }
+    return true;
+};
