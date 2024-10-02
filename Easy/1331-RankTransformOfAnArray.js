33b3bfc30694f3436e3089176476da96a4cc879a
ABNmmd <moha.ww.ha@gmail.com>
2024-10-02 19:16:01 +0100

    Rank Transform of an Array

--- Easy/1331-RankTransformOfAnArray.js
+++ Easy/1331-RankTransformOfAnArray.js
@@ -2,13 +2,13 @@
  * @param {number[]} arr
  * @return {number[]}
  */
-var arrayRankTransform = function(arr) {
+var arrayRankTransform = function (arr) {
     let numRank = {};
-    let sortedArr = [...arr].sort((a, b) => a-b);
+    let sortedArr = [...arr].sort((a, b) => a - b);
     let rank = 1;
 
-    for (let i=0; i<sortedArr.length; i++) {
-        if (i>0 && sortedArr[i] > sortedArr[i-1])
+    for (let i = 0; i < sortedArr.length; i++) {
+        if (i > 0 && sortedArr[i] > sortedArr[i - 1])
             rank++;
         numRank[sortedArr[i]] = rank;
     }
