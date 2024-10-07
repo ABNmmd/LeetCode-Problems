e36f074a0a83e242746a38fc619a614ed055d19b
ABNmmd <moha.ww.ha@gmail.com>
2024-10-07 22:28:11 +0100

    Minimum String Length After Removing Substrings

--- Easy/2696-MinimumStringLengthAfterRemovingSubstrings.js
+++ Easy/2696-MinimumStringLengthAfterRemovingSubstrings.js
@@ -4,11 +4,13 @@
  */
 var minLength = function (s) {
     let stack = [];
+
     for (let c of s) {
         if (!stack.length) {
             stack.push(c);
             continue;
         }
+
         if (c == 'B' && stack[stack.length - 1] == 'A') {
             stack.pop();
         } else if (c == 'D' && stack[stack.length - 1] == 'C') {
@@ -16,7 +18,7 @@ var minLength = function (s) {
         } else {
             stack.push(c);
         }
-
     }
+
     return stack.length;
 }
