1a34c43d35cab46df28bec51e9c96992c5c99a61
ABNmmd <moha.ww.ha@gmail.com>
2024-10-08 21:20:17 +0100

    Minimum Number of Swaps to Make the String

--- Medium/1963-MinimumNumberofSwapstoMaketheString.js
+++ Medium/1963-MinimumNumberofSwapstoMaketheString.js
@@ -4,11 +4,13 @@
  */
 var minSwaps = function (s) {
     let stackSize = 0;
+
     for (c of s) {
         if (c == '[') stackSize++
         else {
             if (stackSize > 0) stackSize--;
         };
     }
+    
     return Math.floor((stackSize + 1) / 2);
 };
