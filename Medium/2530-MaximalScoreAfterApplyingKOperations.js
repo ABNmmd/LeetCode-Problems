11b34a91ca0b3fe9a64d182b4e3442651ae976b7
ABNmmd <moha.ww.ha@gmail.com>
2024-10-14 21:12:54 +0100

    Maximal Score After Applying K Operations

--- Medium/2530-MaximalScoreAfterApplyingKOperations.js
+++ Medium/2530-MaximalScoreAfterApplyingKOperations.js
@@ -3,18 +3,18 @@
  * @param {number} k
  * @return {number}
  */
-var maxKelements = function(nums, k) {
-    let ans =0;
+var maxKelements = function (nums, k) {
+    let ans = 0;
     let pq = new MaxPriorityQueue({ compare: (a, b) => b - a });
 
     for (let num of nums) {
         pq.enqueue(num);
     }
 
-    while(k) {
+    while (k) {
         const el = pq.dequeue();
         ans += el;
-        pq.enqueue(Math.ceil(el/3));
+        pq.enqueue(Math.ceil(el / 3));
         k--;
     }
     return ans;
