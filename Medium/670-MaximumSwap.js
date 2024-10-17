d2655cc1e7bcff45412b6770faf6cc6eba1a0322
ABNmmd <moha.ww.ha@gmail.com>
2024-10-17 23:07:00 +0100

    Maximum Swap

---
+++ Medium/670-MaximumSwap.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximumSwap = function (num) {
+    let numStr = num.toString().split('');
+    let maxDigitIndx = -1;
+    let si1 = -1;
+    let si2 = -1;
+
+    for (let i = numStr.length-1; i >= 0; i--) {
+        if (maxDigitIndx == -1 || numStr[i] > numStr[maxDigitIndx]) maxDigitIndx = i;
+        if (numStr[i] < numStr[maxDigitIndx]) {
+            si1 = i;
+            si2 = maxDigitIndx;
+        }
+    }
+
+    if (si1 != -1 && si2 != -1) {
+        [numStr[si1], numStr[si2]] = [numStr[si2], numStr[si1]];
+    }
+
+    return Number(numStr.join(''));
+};
