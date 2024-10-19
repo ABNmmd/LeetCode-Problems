3866c1d811cbfea2976b8994dafc72a7b261a9b6
ABNmmd <moha.ww.ha@gmail.com>
2024-10-19 21:14:16 +0100

    Find Kth Bit in Nth Binary String

--- Medium/1545-FindKthBitinNthBinaryString.js
+++ Medium/1545-FindKthBitinNthBinaryString.js
@@ -8,8 +8,10 @@ var findKthBit = function (n, k) {
     let len = 2 ** n - 1;
 
     while (k > 1) {
-        if (k == Math.floor(len / 2) + 1) return invertCnt % 2 == 0 ? '1' : '0';
-
+        if (k == Math.floor(len / 2) + 1) {
+            return invertCnt % 2 == 0 ? '1' : '0';
+        }
+        
         if (k > Math.floor(len / 2)) {
             k = len + 1 - k;
             invertCnt++;
