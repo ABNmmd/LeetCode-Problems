e11d2698c7e6756c3a95c2ffa531157f984e5b9e
ABNmmd <moha.ww.ha@gmail.com>
2024-10-20 23:21:51 +0100

    Parsing A Boolean Expression

---
+++ Hard/1106-ParsingABooleanExpression.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string} expression
+ * @return {boolean}
+ */
+var parseBoolExpr = function (expression) {
+    let st = [];
+
+    for (let currChar of expression) {
+        if (currChar == ')') {
+            let val = [];
+
+            while (st[st.length - 1] != '(') {
+                val.push(st.pop());
+            }
+            st.pop();
+            let op = st.pop();
+
+            let res = evaluateSubExp(op, val);
+            st.push(res);
+        } else if (currChar != ',') {
+            st.push(currChar);
+        }
+    }
+
+    return st[st.length - 1] == 't';
+};
+
+var evaluateSubExp = function (op, vals) {
+    if (op == '!') return vals[0] == 't' ? 'f' : 't';
+
+    if (op == '&') {
+        for (let val of vals) {
+            if (val == 'f') return 'f';
+        }
+        return 't';
+    }
+
+    if (op == '|') {
+        for (let val of vals) {
+            if (val == 't') return 't';
+        }
+        return 'f';
+    }
+
+    return 'f';
+}
