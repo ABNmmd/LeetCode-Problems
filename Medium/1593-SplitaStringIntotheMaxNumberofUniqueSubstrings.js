eed27ac5103adcd85fbfc8d638737c59a38a9342
ABNmmd <moha.ww.ha@gmail.com>
2024-10-21 23:06:15 +0100

    Split a String Into the Max Number of Unique Substrings

---
+++ Medium/1593-SplitaStringIntotheMaxNumberofUniqueSubstrings.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxUniqueSplit = function(s) {
+    let seen = new Set();
+    return backtrack(s, 0, seen);
+};
+
+var backtrack = function(s, start, seen) {
+    if (start == s.length) return 0;
+    let maxCnt = 0;
+
+    for (let end = start + 1; end <= s.length; end++) {
+        let subStr = s.substring(start, end);
+        if (!seen.has(subStr)) {
+            seen.add(subStr);
+            maxCnt = Math.max(maxCnt, 1 + backtrack(s, end, seen));
+            seen.delete(subStr);
+        }
+    }
+    return maxCnt;
+}
